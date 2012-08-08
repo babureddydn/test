12aac93aad90c4c889253007d1fe57cc9d265cf9
Babu Reddy <breddy@collective.com>
2012-08-08 12:45:37 +0530

    Testing...

---
+++ Misc.java
@@ -0,0 +1,75 @@
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Misc implements Runnable {
+    private String url ="http://ww.google.com";
+    private static int i=0;
+    public Misc(String u){
+        url = u;
+    }
+    public static void main(String[] args)throws Exception{
+        new Thread(new Misc("http://www.google.com/")).start();
+    }
+    private static void retrieveBinaryData(String targetUrl) throws IOException {
+        URL url;
+                URLConnection urlConnection = null;
+        DataInputStream dataInputStream = null;
+        byte []imageBytes;
+        int initialSizeLimit = 2000;
+        int nextSizeLimit = 2000;
+        try {
+            url = new URL(targetUrl);
+            urlConnection = url.openConnection();
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+            urlConnection.setUseCaches(false);
+            urlConnection.setConnectTimeout(1000 * 10);
+            urlConnection.setReadTimeout(1000 * 60 * 5);
+
+            imageBytes = new byte[initialSizeLimit];
+            dataInputStream = new DataInputStream(urlConnection.getInputStream());
+
+             while(dataInputStream.available()>0) {
+                dataInputStream.read(imageBytes, 0,nextSizeLimit);
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(dataInputStream != null) {
+                    dataInputStream.close ();
+                    dataInputStream = null;
+                }
+            }catch(Exception e){
+                dataInputStream = null;
+                e.printStackTrace();
+            }
+            try {
+                if(urlConnection != null) {
+                    ((HttpURLConnection)urlConnection).disconnect();
+                    System.out.println("Url closed");
+                }
+            }catch(Exception e){
+                urlConnection = null;
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+        	long startTime = System.currentTimeMillis();
+            retrieveBinaryData(url);
+        	long endTime = System.currentTimeMillis();
+			System.out.println("Time taken:" + (endTime - startTime) + " ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
